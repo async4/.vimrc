af504e3909218effc2504c7546a979e28a28febb
sergen ☕️ <53083523+csergen@users.noreply.github.com>
2021-03-02 18:37:27 +0300

    Add files via upload

---
+++ mylib.h
@@ -0,0 +1,86 @@
+/** 
+*	sergen cepoglu
+* github.com/csergen
+*/
+
+#ifndef __MYLIB_H__
+#define __MYLIB_H__
+
+#if defined (_WIN32) || defined (_WIN64) || defined (__WINDOWS__) || defined (__CYGWIN__)
+#define _CRT_SECURE_NO_DEPRECATE
+#elif defined (__linux__)
+#define _CRT_SECURE_NO_DEPRECATE
+#elif defined (__APPLE__) || (__MACH__)
+#define _CRT_SECURE_NO_DEPRECATE
+#elif defined (unix) || defined (__unix__) || defined (__unix)
+#define _CRT_SECURE_NO_DEPRECATE
+#else
+#error Unknown
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define False 0x00
+#define True  0x01
+#define String char*
+
+#define DEBUG True
+
+#if DEBUG == True
+#define Logi(_int_arg) printf("%d\n", _int_arg)
+#define Logc(_char_arg) printf("%c\n", _char_arg)
+#define Log(_format, ...) fprintf(stderr, _format, ##__VA_ARGS__)
+#else
+#define Logi(void)
+#define Logc(void)
+#define Log(_format, ...)
+#endif
+
+#define Min(x, y) ((x) < (y) ? (True) : (False))
+
+#define Max(x, y) ((x) > (y) ? (True) : (False))
+
+#define New(_type_arg, size) malloc(sizeof(_type_arg)*(size));
+
+#define Size(_array_arg) (sizeof(_array_arg) / sizeof((_array_arg)[0]))
+
+
+// I/O
+FILE* Open(String _file_path, String _file_mode)
+{
+	return fopen(_file_path, _file_mode);
+}
+
+int Close(FILE* _file)
+{
+	int status = True;
+
+	if (_file != NULL) 
+		status = (fclose(_file) == NULL) ? True : False;
+	
+	if (status == True)
+		free(_file);
+	else
+		Log("File is already closed.");
+
+	return status;
+}
+
+int Write(FILE* _file, String _text)
+{
+	if (_file == NULL) {
+		Log("File is not opened.");
+		return False;
+	} 
+	
+	fputs(_file, _text);
+
+	return True;
+}
+
+
+
+
+#endif
