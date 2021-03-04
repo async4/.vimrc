4d7823748bd98172f25f89676b889544080b1f7f
sergen ☕️ <53083523+csergen@users.noreply.github.com>
2021-03-04 10:45:46 +0300

    Add files via upload

--- mylib.h
+++ mylib.h
@@ -7,53 +7,60 @@
 #define __MYLIB_H__
 
 #if defined (_WIN32) || defined (_WIN64) || defined (__WINDOWS__) || defined (__CYGWIN__)
-#define _CRT_SECURE_NO_DEPRECATE
+	#define _CRT_SECURE_NO_DEPRECATE
 #elif defined (__linux__)
-#define _CRT_SECURE_NO_DEPRECATE
+	#define _CRT_SECURE_NO_DEPRECATE
 #elif defined (__APPLE__) || (__MACH__)
-#define _CRT_SECURE_NO_DEPRECATE
+	#define _CRT_SECURE_NO_DEPRECATE
 #elif defined (unix) || defined (__unix__) || defined (__unix)
-#define _CRT_SECURE_NO_DEPRECATE
+	#define _CRT_SECURE_NO_DEPRECATE
 #else
-#error Unknown
+	#error Unknown
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 #define False 0x00
-#define True  0x01
-#define String char*
+#define True	!False
 
 #define DEBUG True
 
+#define String char*
+
+
+static const BUFFER = 0x3E8;
+
 #if DEBUG == True
-#define Logi(_int_arg) printf("%d\n", _int_arg)
-#define Logc(_char_arg) printf("%c\n", _char_arg)
-#define Log(_format, ...) fprintf(stderr, _format, ##__VA_ARGS__)
+	#define Logi(_int_arg) printf("%d\n", _int_arg)
+	#define Logc(_char_arg) printf("%c\n", _char_arg)
+	#define Log(_format, ...) fprintf(stderr, _format, ##__VA_ARGS__)
 #else
-#define Logi(void)
-#define Logc(void)
-#define Log(_format, ...)
+	#define Logi(void)
+	#define Logc(void)
+	#define Log(_format, ...)
 #endif
+
 
 #define Min(x, y) ((x) < (y) ? (True) : (False))
 
 #define Max(x, y) ((x) > (y) ? (True) : (False))
 
-#define New(_type_arg, size) malloc(sizeof(_type_arg)*(size));
+#define Malloc(_type_arg, _size) malloc(sizeof(_type_arg)*(_size));
+#define Calloc(_type_arg, _size) calloc(_size, sizeof(_type_arg));
 
 #define Size(_array_arg) (sizeof(_array_arg) / sizeof((_array_arg)[0]))
 
 
-// I/O
-FILE* Open(String _file_path, String _file_mode)
+// FILE HANDLING
+FILE* Open(const String const _file_path, const String const _file_mode)
 {
 	return fopen(_file_path, _file_mode);
 }
 
-int Close(FILE* _file)
+int Close(const FILE* const _file)
 {
 	int status = True;
 
@@ -68,19 +75,35 @@ int Close(FILE* _file)
 	return status;
 }
 
-int Write(FILE* _file, String _text)
+int Write(const FILE* const _file, const String const _text)
 {
 	if (_file == NULL) {
 		Log("File is not opened.");
 		return False;
 	} 
 	
-	fputs(_file, _text);
+	fprintf(_file, "%s", _text);
 
 	return True;
 }
 
+int Read(const FILE* const _file, const String const _destination)
+{
+	String line = Calloc(String, BUFFER);
 
+	while (fscanf(_file, "%[^\n] ", line) == 1)
+	{
+		strcat(line, "\n");
+		strcat(_destination, line);
+	}
+	
+	free(line);
+
+	if (_destination[0] == NULL)
+		return False;
+
+	return True;
+}
 
 
 #endif
